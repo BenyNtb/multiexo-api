ce7bafaa75977a986f3a3bafc63dd9b6cbe0b7f7
Beny <b.ntambwe97@gmail.com>
2021-12-20 16:46:42 +0100

    exo 1 done, css missing

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div>
+      <div class="cart_list">
+        <div >
+          <div class="cart_item"  v-for="food in shop"
+      :key="food.id">
+            <img :src="url+food.cover_path" alt="">
+            <span>{{ food.name }}</span>
+            <span>Description: {{ food.description }}</span>
+            <span> {{food.price }} €</span>
+            <div>
+              <button
+                type="button"
+                class="button item_button cart_button"
+                @click="addToCart(p)"
+              >
+                +
+              </button>
+              <button
+                type="button"
+                class="button item_button remove_button"
+                @click="removeFromCart(p)"
+              >
+                &minus;
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      shop: null,
+      url: 'https://api-moshop.molengeek.pro/'
+
+    }
+  },
+  mounted () {
+    axios.get('https://api-moshop.molengeek.pro/api/v1/mg/shop')
+      .then(res => this.shop = res.data.data.products)
+  }
+}
+</script>
